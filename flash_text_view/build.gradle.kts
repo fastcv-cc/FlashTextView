e58873053dee496c85762443db784d78549d3fb1
pisto <heyxhazx1003@163.com>
2025-06-07 11:04:00 +0800

    feat(app): add FlashTextView
    add FlashTextView
    
    BREAKING CHANGE: null

--- flash_text_view/build.gradle.kts
+++ flash_text_view/build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "cc.fastcv.flashtextview"
+    namespace = "cc.fastcv.flash_text_view"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "cc.fastcv.flashtextview"
         minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -40,7 +37,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(project(":flash_text_view"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "FlashTextView"
 include(":app")
- 
+include(":flash_text_view")
